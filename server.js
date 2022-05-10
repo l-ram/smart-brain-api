2f088f06d12bcf58a6f2f8941355f79d2769554c
l-ram <larryramjutton@gmail.com>
2022-05-10 15:43:04 +0200

    for heroku deployment

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser'); // latest version of exressJS now comes with Body-Parser!
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+const knex = require('knex')
+
+const db = knex({
+client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'test',
+    database : 'smart-brain'
+  }
+});
+
+console.log(db.select('*').from('users').then(data=>
+    console.log(data)
+    ));
+
+
+const app = express();
+
+app.use(cors())
+app.use(express.json()); // latest version of exressJS now comes with Body-Parser!
+
+app.get('/', (req, res)=> { res.send(db.users) })
+app.post('/signin', signin.handleSignin(db, bcrypt))
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+
+// app.listen(3000, ()=> {
+//   console.log('app is running on port 3000');
+// })
+
+const PORT = process.env.PORT
+
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+console.log(PORT)

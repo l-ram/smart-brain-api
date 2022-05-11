8167b21de4efd942d57424ac7129340bf397c718
l-ram <larryramjutton@gmail.com>
2022-05-11 12:48:32 +0200

    db

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const knex = require('knex')
 const db = knex({
 client: 'pg',
   connection: {
-    host : 'postgresql-opaque-37753',
-    user : 'postgres',
-    password : 'test',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
